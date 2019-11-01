4d997e5069790a83d7badda54b8ea943935f963c
Jeremy T Hatcher <jeremyhatcher27@gmail.com>
2019-11-01 14:45:50 -0300

    string table section contents are now parsed as Strings
    incremented version patch number

--- src/constants.rs
+++ src/constants.rs
@@ -1,5 +1,9 @@
+use crate::primitives::*;
+
 /**
  * Various constants defined by both the ELF standard as well as various machine vendors used to parse ELF files
  */
 
-pub const ELF_MAGIC_BYTES: [u8; 4] = [0x7F, 0x45, 0x4C, 0x46];
+pub const MAGIC_BYTES: [u8; 4] = [0x7F, 0x45, 0x4C, 0x46];
+pub const SHN_UNDEF: Short = Short(0);
+
